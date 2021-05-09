cb45e15a8a84c6478d13041d27ed77e150cc53b7
Martet <martin@zmitko.cz>
2021-05-09 21:01:45 +0200

    Added model fragment and vertex shaders - Test 36-37

--- student/drawModel.cpp
+++ student/drawModel.cpp
@@ -7,6 +7,8 @@
 #include <student/drawModel.hpp>
 #include <student/gpu.hpp>
 #include <stdio.h>
+#include <iostream>
+#include "../tests/testCommon.hpp"
 
 void drawNode(GPUContext &ctx, Node const &node, Model const &model, glm::mat4 mat){
   if(node.mesh >= 0){ // ma tento node mesh?
@@ -70,12 +72,10 @@ void drawModel(GPUContext &ctx, Model const &model, glm::mat4 const &proj, glm::
  */
 //! [drawModel_vs]
 void drawModel_vertexShader(OutVertex&outVertex,InVertex const&inVertex,Uniforms const&uniforms){
-  (void)outVertex;
-  (void)inVertex;
-  (void)uniforms;
-  /// \todo Tato funkce reprezentujte vertex shader.<br>
-  /// Vaším úkolem je správně trasnformovat vrcholy modelu.
-  /// Bližší informace jsou uvedeny na hlavní stránce dokumentace.
+  outVertex.attributes[0].v3 = uniforms.uniform[1].m4 * glm::vec4(inVertex.attributes[0].v3, 1.f);
+  outVertex.attributes[1].v3 = uniforms.uniform[2].m4 * glm::vec4(inVertex.attributes[1].v3, 0.f);
+  outVertex.attributes[2].v2 = inVertex.attributes[2].v2;
+  outVertex.gl_Position = uniforms.uniform[0].m4 * uniforms.uniform[1].m4 * glm::vec4(inVertex.attributes[0].v3, 1.f);
 }
 //! [drawModel_vs]
 
@@ -88,12 +88,11 @@ void drawModel_vertexShader(OutVertex&outVertex,InVertex const&inVertex,Uniforms
  */
 //! [drawModel_fs]
 void drawModel_fragmentShader(OutFragment&outFragment,InFragment const&inFragment,Uniforms const&uniforms){
-  (void)outFragment;
-  (void)inFragment;
-  (void)uniforms;
-  /// \todo Tato funkce reprezentujte fragment shader.<br>
-  /// Vaším úkolem je správně obarvit fragmenty a osvětlit je pomocí lambertova osvětlovacího modelu.
-  /// Bližší informace jsou uvedeny na hlavní stránce dokumentace.
+  glm::vec4 tex = uniforms.uniform[6].v1 > 0.f ? read_texture(uniforms.textures[0], inFragment.attributes[2].v2) : uniforms.uniform[5].v4;
+  glm::vec3 aL = tex * 0.2f;
+  glm::vec3 dL, L = uniforms.uniform[3].v3, N = inFragment.attributes[1].v3;
+  dL = tex * glm::clamp(glm::dot(L - inFragment.attributes[0].v3, glm::normalize(N)), 0.f, 1.f);
+  outFragment.gl_FragColor = glm::vec4(aL + dL, tex.a);
 }
 //! [drawModel_fs]
 
